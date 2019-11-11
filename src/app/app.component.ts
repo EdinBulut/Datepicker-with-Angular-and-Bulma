2d272f81096aff9ca5a1ebc9a380996c9c6795e2
EdinBulut <edin.bulut@hotmail.com>
2019-11-11 22:34:32 +0100

    Adds datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'datepicker';
+export class AppComponent implements OnInit {
+
+  locale = 'en';
+  selectedDate: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setSelectedDate(date) {
+    this.selectedDate = date;
+  }
+
+  canChangeMonthLogic(num, currentDate) {
+    currentDate.add(num, 'month');
+    const minDate = moment().add(-3, 'year');
+    const maxDate = moment().add(3, 'year');
+    return currentDate.isBetween(minDate, maxDate);
+  }
+
+  isAvailableLogic(dateToCheck: any) {
+    if (dateToCheck.isBefore(moment(), 'day')) {
+      return false;
+      } else {
+          return true;
+      }
+  }
+
 }
+
